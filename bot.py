866d6bf3b93ac8f425e7f32012411b1bc92d4989
uiopyt12 <108012256+uiopyt12@users.noreply.github.com>
2024-04-30 13:14:44 +0000

    제출

--- bot.py
+++ bot.py
@@ -1,26 +1,24 @@
 import random
 
 # This list contains the random responses (you can add your own or translate them into your own language too)
-random_responses = ["That is quite interesting, please tell me more.",
-                    "I see. Do go on.",
-                    "Why do you say that?",
-                    "Funny weather we've been having, isn't it?",
-                    "Let's change the subject.",
-                    "Did you catch the game last night?"]
+random_responses = ["꽤 흥미롭네요, 더 말해봐요.",
+                    "그렇군요. 계속하세요.",
+                    "왜 그렇게 말하시는 거예요?",
+                    "날씨가 좋네요, 그렇죠?",
+                    "다른 주제는 어때요?.",
+                    "어젯밤에 했던 게임은 좀 어때요?"]
 
-print("Hello, I am Marvin, the simple robot.")
-print("You can end this conversation at any time by typing 'bye'")
-print("After typing each answer, press 'enter'")
-print("How are you today?")
+print("안녕하세요 저는 간단한 챗봇 마빈입니다.")
+print("대화를 종료하고 싶으시다면 '안녕'이라고 해주세요.")
+print("대답을 입력한 후에는 엔터를 눌러 전송해주세요.")
+print("오늘은 좀 어때요?")
 
 while True:
-    # wait for the user to enter some text
     user_input = input("> ")
-    if user_input.lower() == "bye":
-        # if they typed in 'bye' (or even BYE, ByE, byE etc.), break out of the loop
+    if user_input == "안녕":
         break
     else:
         response = random.choices(random_responses)[0]
     print(response)
 
-print("It was nice talking to you, goodbye!")
+print("좋은 대화였어요! 안녕히 가세요!")
